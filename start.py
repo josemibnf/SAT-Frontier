a3c0c1473bdf06aff678b6f80e38bb3a67d56359
josemibnf <josemi.bnf@outlook.es>
2021-01-14 18:10:48 +0100

    proto

--- start.py
+++ start.py
@@ -1,5 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
+import io
 import frontier
+from api_pb2 import *
 
 app = Flask(__name__)
 
@@ -7,8 +9,13 @@ if __name__ == "__main__":
 
     @app.route('/', methods=['GET', 'POST'])
     def post():
-        cnf = request.json.get('cnf')
+        cnf = Cnf.ParseFromString(request.json.get('cnf'))
         solution = frontier.ok(cnf=cnf)
-        return {'interpretation':solution}
+        return send_file(
+            io.BytesIO(Interpretation.SerializeToString(solution)),
+            as_attachment=True,
+            attachment_filename='abc.abc',
+            mimetype='attachment/x-protobuf'
+        )
 
     app.run(host='0.0.0.0', port=8080)
